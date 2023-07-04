418a349cdbdc12e04695053f6a9821b43824cf38
Jaraxzus <alex099024@gmail.com>
2023-07-04 19:42:47 +0300

    add redis-cashing-support

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -198,6 +198,6 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/1",
     }
 }
